5a6bb74aedcd94b047e862434d535b8cd2a95bea
YuriyRe <GITHUB yuriyreutov@gmail.com>
2018-09-09 18:28:25 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.print("Основная информацияя\b");
+	System.out.print(" о");
+	System.out.print(" студенте:");
+	 System.out.println("\n\tИмя: \tФамилия: \tГруппа:");
+	 System.out.println("\tЮрий  \tРеутов    \tАТ-162");
+    }
+}
